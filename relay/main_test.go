2e3cf4f62309909b0b80b8ed052f9a85bcbd869a
clubvine44-gif <clubvine44@gmail.com>
2026-09-10 09:51:58 +0300

    test(relay): avoid close-frame pipe race in bridge test

--- relay/main_test.go
+++ relay/main_test.go
@@ -111,9 +111,9 @@ func (t *fakeTUN) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
-func (t *fakeTUN) Close() error                             { return nil }
+func (t *fakeTUN) Close() error { return nil }
 func (t *fakeTUN) SetReadDeadline(deadline time.Time) error { return nil }
-func (t *fakeTUN) stop()                                    { close(t.readCh) }
+func (t *fakeTUN) stop() { close(t.readCh) }
 
 func TestTunnelClientBridgesBothDirections(t *testing.T) {
 	client, server := net.Pipe()
@@ -148,9 +148,6 @@ func TestTunnelClientBridgesBothDirections(t *testing.T) {
 		t.Fatalf("return frame mismatch: type=%d payload=%x", gotFrame.typ, gotFrame.payload)
 	}
 
-	if err := writeFrame(client, typeClose, nil); err != nil {
-		t.Fatal(err)
-	}
 	client.Close()
 	tun.stop()
 	select {
